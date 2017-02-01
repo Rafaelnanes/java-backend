9f4d61f99f3d6c18158fedb179f3de64bd89145c
Rafael Nanes <rafaelbraga_22@hotmail.com>
2017-01-31 22:20:24 -0300

    ajuste para publicação no heroku

--- api/src/main/java/rbn/edu/config/ApplicationInitializer.java
+++ api/src/main/java/rbn/edu/config/ApplicationInitializer.java
@@ -5,25 +5,25 @@ import javax.servlet.Filter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class ApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-    @Override
-    protected Class<?>[] getRootConfigClasses() {
-	return new Class<?>[] { ApplicationConfig.class };
-    }
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[] { ApplicationConfig.class };
+	}
 
-    @Override
-    protected Class<?>[] getServletConfigClasses() {
-	// when use this ? when apply different servlets in application?
-	// return new Class<?>[] { WebConfig.class };
-	return null;
-    }
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// when use this ? when apply different servlets in application?
+		// return new Class<?>[] { WebConfig.class };
+		return null;
+	}
 
-    @Override
-    protected Filter[] getServletFilters() {
-	return new Filter[] {};
-    }
+	@Override
+	protected Filter[] getServletFilters() {
+		return new Filter[] {};
+	}
 
-    @Override
-    protected String[] getServletMappings() {
-	return new String[] { "/" };
-    }
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
 }
