34808a3d0d307d8145464484bd21558cc9df5019
rafael nanes <rafaelbraga_22@hotmail.com>
2017-08-08 20:17:26 -0300

    fixing the problem properties by profile

--- dao-impl-hibernate/src/main/java/rbn/edu/config/HibernateConfiguration.java
+++ dao-impl-hibernate/src/main/java/rbn/edu/config/HibernateConfiguration.java
@@ -17,7 +17,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
-@PropertySource(value = { "classpath:application.properties" })
+@PropertySource(value = { "classpath:db.properties" })
 public class HibernateConfiguration {
 
     @Autowired
